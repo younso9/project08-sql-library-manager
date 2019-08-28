e674d83397ca2a20b7d7e79d2b57e760b277226d
younso9 <sonya.youngblood@verizon.com>
2019-08-28 15:09:42 -0400

    do over
    create repo

---
+++ models/book.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+    title: {
+      type: DataTypes.STRING,
+      //notEmpty is validate, custom error message shown on the respective form
+      validate: { 
+        notEmpty: {
+          msg: "Title cannot be empty."
+        }
+      },
+    },
+    author: {
+      type: DataTypes.STRING,
+      //notEmpty is validate, custom error message shown on the respective form
+      validate: {
+        notEmpty: {
+          msg: "Author cannot be empty."
+        }
+      },
+    },
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {});
+  Book.associate = function(models) {
+    // associations can be defined here
+  };
+  return Book;
+};
